501762353d179c0abea0c7f930c152bdd9de8c57
Alex Podprugin <alexpod4321@gmail.com>
2026-06-03 00:46:44 +0300

    Update main_window.py
    исправил

--- gui/main_window.py
+++ gui/main_window.py
@@ -6,7 +6,7 @@ class MainWindow:
     def __init__(self, user_id, session_id, session_obj=None):
         self.user_id = user_id
         self.session_id = session_id
-        self.session_obj = session_obj   # можешь оставить, если нужен для других целей
+        self.session_obj = session_obj   
         logger.info('MainWindow initialized')
 
     def run(self):
